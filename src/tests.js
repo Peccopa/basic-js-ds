c5f1477c59f77cfdfc9e64f1b471d9ed56f1f08a
Andrey Zharkikh <orbital.logistics@yandex.ru>
2025-10-23 13:43:26 +0300

    feat: add task solution: queue

--- src/tests.js
+++ src/tests.js
@@ -1,22 +1,35 @@
-class Queue {
-  #queue = [];
+// const { ListNode } = require('../extensions/list-node.js');
 
-  getUnderlyingList() {
+// class Queue {
+//   #head = null;
+//   #tail = null;
 
-  }
+//   getUnderlyingList() {
+//     return this.#head;
+//   }
 
-  enqueue(/* value */) {
+//   enqueue(value) {
+//     const node = new ListNode(value);
+//     if (this.#head) {
+//       this.#tail.next = node;
+//       this.#tail = node;
+//     } else {
+//       this.#head = node;
+//       this.#tail = node;
+//     }
+//   }
 
-  }
+//   dequeue() {
+//     if (!this.#head) return undefined;
+//     const head = this.#head.value;
+//     this.#head = this.#head.next;
+//     return head;
+//   }
+// }
 
-  dequeue() {
-
-  }
-}
-
-const queue = new Queue();
-console.log(queue);
-console.log(queue.enqueue(1)); // adds the element to the queue
-console.log(queue.enqueue(3)); // adds the element to the queue
-console.log(queue.dequeue()); // returns the top element from queue and deletes it, returns 1
-console.log(queue.getUnderlyingList()); // returns { value: 3, next: null }
+// const queue = new Queue();
+// console.log(queue);
+// console.log(queue.enqueue(1)); // adds the element to the queue
+// console.log(queue.enqueue(3)); // adds the element to the queue
+// console.log(queue.dequeue()); // returns the top element from queue and deletes it, returns 1
+// console.log(queue.getUnderlyingList()); // returns { value: 3, next: null }
